37a7b12164ac899b212933de63eeba4f6783a318
Adityabhogil <Adityabhogil@gmail.com>
2023-07-30 08:09:48 +0530

    intial2

--- App.tsx
+++ App.tsx
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { Text, View, Pressable } from "react-native";
+ 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Pressable>
+        <Text className=" bg-blue-500 hover:bg-blue-700 text-pink-300 font-bold py-2 px-4 rounded-full">
+          Login using Vesid
+        </Text>
+      </Pressable>
+
+      <Text className="text--500 font-bold  ">
+        Welcome to the vesitguide
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
