bdabe8b80786d5de21fabda27b84cd3aedbea777
Adityabhogil <Adityabhogil@gmail.com>
2023-07-31 01:49:02 +0530

    newfolder

--- App/welcome.tsx
+++ App/welcome.tsx
@@ -10,11 +10,11 @@ export default function welcome() {
       ></Image>
 
       <Text className="text--500 font-bold text-3xl">
-        Welcome to the vestguide
+        Welcome to the vesitguide
       </Text>
       <Pressable>
-        <Text className="bg-purple-500 hover:bg-purple-400 text-white font-bold py-2 px-4 border-b-4 border-purple-700 hover:border-purple-500 rounded text-2xl mt-20">
-          Login using Vesid
+        <Text className="bg-purple-400 hover:bg-purple-400 text-white font-bold py-2 px-4 border-b-4 border-purple-400 hover:border-purple-500 rounded text-2xl mt-20">
+          Login using Vesid 
         </Text>
       </Pressable>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  content: ["./welcome.{js,jsx,ts,tsx}"],
+  content: ["./App/welcome.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
