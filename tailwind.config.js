e3427f19404b3ae401c30d63993a044c93659a50
Adityabhogil <Adityabhogil@gmail.com>
2023-07-30 22:09:33 +0530

    edit2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}"],
+  content: ["./welcome.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
--- welcome.tsx
+++ welcome.tsx
@@ -10,7 +10,7 @@ export default function welcome() {
       ></Image>
 
       <Text className="text--500 font-bold text-3xl">
-        Welcome to the vesitguide
+        Welcome to the vestguide
       </Text>
       <Pressable>
         <Text className="bg-purple-500 hover:bg-purple-400 text-white font-bold py-2 px-4 border-b-4 border-purple-700 hover:border-purple-500 rounded text-2xl mt-20">
