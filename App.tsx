c383e37b14dabb94c2d5eb2822f9d1bb3c2f9a7b
Adityabhogil <Adityabhogil@gmail.com>
2023-07-30 16:38:08 +0530

    img

--- App.tsx
+++ App.tsx
@@ -1,18 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View, Pressable } from "react-native";
- 
+import { Text, View, Pressable,Image} from "react-native";
+
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-green-200">
+    <View className="flex-1 items-center justify-center bg-orange-200">
+      <Image source={require("/Users/adityabhogil/vesitguide/assets/logo.jpeg")} className=""></Image>
+      
+      <Text className="text--500 font-bold text-3xl">Welcome to the vesitguide</Text>
       <Pressable>
-        <Text className=" bg-blue-500 hover:bg-blue-700 text-pink-300 font-bold py-2 px-4 rounded-full">
-          Login using Vesit
+        <Text className="bg-purple-500 hover:bg-purple-400 text-white font-bold py-2 px-4 border-b-4 border-purple-700 hover:border-purple-500 rounded text-2xl mt-20">
+          Login using Vesid
         </Text>
       </Pressable>
 
-      <Text className="text--500 font-bold  ">
-        Welcome to the vesitguide
-      </Text>
       <StatusBar style="auto" />
     </View>
   );
