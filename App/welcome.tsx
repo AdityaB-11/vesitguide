47152069ad21e713f27ccdf79019101cec998617
Adityabhogil <Adityabhogil@gmail.com>
2023-07-31 16:25:01 +0530

    bg-change

--- App/welcome.tsx
+++ App/welcome.tsx
@@ -3,14 +3,14 @@ import { Text, View, Pressable, Image } from "react-native";
 
 export default function welcome() {
   return (
-    <View className="flex-1 items-center justify-center bg-orange-200">
+    <View className="flex-1 items-center justify-center bg-orange-100">
       <Image
         source={require("/Users/adityabhogil/vesitguide/assets/logo.png")}
         className="w-1/3 h-1/6 rounded-3xl "
       ></Image>
 
       <Text className="text--500 font-bold text-3xl">
-        Welcome to the vesitguide
+        Welcome to the Vesitguide
       </Text>
       <Pressable>
         <Text className="bg-purple-400 hover:bg-purple-400 text-white font-bold py-2 px-4 border-b-4 border-purple-400 hover:border-purple-500 rounded text-2xl mt-20">
