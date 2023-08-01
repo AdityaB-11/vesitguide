da9b22fd61f33f85502e1f73be6bdc22631ca0d3
Adityabhogil <Adityabhogil@gmail.com>
2023-08-01 22:57:25 +0530

    new-commit

--- App/welcome.tsx
+++ App/welcome.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View, Pressable, Image } from "react-native";
 
-export default function welcome() {
+export default function Welcome() {
   return (
     <View className="flex-1 items-center justify-center bg-orange-100">
       <Image
