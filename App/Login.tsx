c22e1242a69aa868d894157eca5041627123857e
Adityabhogil <Adityabhogil@gmail.com>
2023-08-01 21:34:34 +0530

    login-first-edit

--- App/Login.tsx
+++ App/Login.tsx
@@ -1,17 +1,35 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View, Pressable,Image} from "react-native";
+import { useState } from "react";
+import { Text, View, Pressable, Image, TextInput } from "react-native";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
   return (
-    <view className="">
-        <text className="">
+    <View className="flex-1 items-center justify-center bg-orange-100">
+      <Image
+        source={require("vesitguide/assets/logo.png")}
+        className="w-1/3 h-1/6 rounded-3xl "
+      ></Image>
 
-            
-        </text>
+      <Text className="text--500 font-bold text-3xl mt-">Login Via VesID</Text>
+      <View className="items-center justify-center">
+        <TextInput
+          placeholder="Enter the vesid"
+          autoFocus
+          value={email}
+          onChangeText={setEmail}
+          className="border border-grey-400 p-3 rounded-xl mt-5 w-9/10"
+        ></TextInput>
 
+        <TextInput
+          placeholder=" Enter the password"
+          value={email}
+          onChangeText={setEmail}
+          className="border border-grey-400 p-3 rounded-xl mt-5 w-9/10"
+        ></TextInput>
+      </View>
 
-
-    </view>
-  
+      <StatusBar style="auto" />
+    </View>
   );
 }
